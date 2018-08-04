06059b728b6a2662620986339bc87ef248b85c85
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 07:06:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(arrI, arrM) {
   var empty = []
-  for (i = 0; i < arrM.length; i++) {
+  for (var i = 0; i < arrM.length; i++) {
     empty.push(`${arrM[i]} plays ${arrI[i]}`)
   }
 
