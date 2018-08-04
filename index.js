72daa0f7e45d08ac3f1b16edd97857c12c334066
Vishal Sharma <vsh1819@gmail.com>
2018-08-04 07:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(arrI, arrM) {
   var empty = []
   for (var i = 0; i < arrM.length; i++) {
-    empty.push(`${arrM[i]} plays ${arrI[i]}`)
+    empty.push(`${arrI[i]} plays ${arrM[i]}`)
   }
 
   return empty
